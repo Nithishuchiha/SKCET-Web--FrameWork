5520f1b784d4a6d8cc1e31456088e77f88852165
Nithishuchiha <124854933+Nithishuchiha@users.noreply.github.com>
2024-02-13 21:07:04 +0530

    class_exercise_3 updated

--- Day-1/class_exercise_3/src/main/java/com/example/class_exercise_3/controller/ApiController.java
+++ Day-1/class_exercise_3/src/main/java/com/example/class_exercise_3/controller/ApiController.java
@@ -2,7 +2,7 @@ package com.example.class_exercise_3.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
